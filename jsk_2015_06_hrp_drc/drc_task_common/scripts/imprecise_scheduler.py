a786490d12e82ab71469dcb072f93924cec3c65b
Ryohei Ueda <garaemon@gmail.com>
2016-01-04 23:34:58 +0900

    [drc_task_common] Fix profile_data column name

--- jsk_2015_06_hrp_drc/drc_task_common/scripts/imprecise_scheduler.py
+++ jsk_2015_06_hrp_drc/drc_task_common/scripts/imprecise_scheduler.py
@@ -16,19 +16,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--type', default="valve_inplace", type=str)
 parser.add_argument('--p0', default=1.0, type=float)
 parser.add_argument('--m0', default=1.0, type=float)
-parser.add_argument('--e0', default=0.0, type=float)
+parser.add_argument('--e0', default=4.0, type=float)
 parser.add_argument('--p1', default=1.0, type=float)
 parser.add_argument('--p-tracking', default=0.0, type=float)
 parser.add_argument('--m1', default=1.0, type=float)
 parser.add_argument('--e1', default=1.0, type=float)
-parser.add_argument('sigma', type=float)
+parser.add_argument('time', type=float)
 parser.add_argument('--wait', action="store_true")
 parser.add_argument('--quiet', action="store_true")
 parser.add_argument('--incremental', action="store_true")
+parser.add_argument('--distance', type=float)
 args = parser.parse_args()
-deadline_sigma = args.sigma
+deadline_time = args.time
 
-if args.type not in ["valve_inplace"]:
+if args.type not in ["valve_inplace", "door_inplace", "valve_walk", "door_walk"]:
     raise Exception("Unknown type: %s" % (args.type))
 
 container = OptimizationContainer()
@@ -39,13 +40,77 @@ if args.type == "valve_inplace":
     initial_speed_factor = 5
     p_table = QualityTable("q_{p0}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", args.p0)
     m_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q_mono.csv", args.m0)
-    m_table_all = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q.csv", args.m0)
-    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/valve/jaxon_red_valve_zmp_ee.csv", args.e0)
-    container.register_quality_table(p_table, p_table.lookup_value('dx', initial_dx, 'sigma'))
+    m_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q.csv", args.m0)
+    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/valve/jaxon_red_valve_zmp_ee_normalized.csv", 1)
+    container.register_quality_table(p_table, p_table.lookup_value('dx', initial_dx, 'time'))
     container.register_quality_table(m_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
                                                                         initial_collision, initial_traj,
                                                                         'time'))
     container.register_quality_table(e_table, e_table.lookup_value('speed-factor', initial_speed_factor, 'time'))
+elif args.type == "door_inplace":
+    initial_dx = 10
+    initial_collision = 10
+    initial_traj = 20
+    initial_speed_factor = 5
+    p_table = QualityTable("q_{p0}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", args.p0)
+    m_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q_mono.csv", args.m0)
+    m_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q.csv", args.m0)
+    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/door/jaxon_red_door_zmp_ee_normalized.csv", 1)
+    container.register_quality_table(p_table, p_table.lookup_value('dx', initial_dx, 'time'))
+    container.register_quality_table(m_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
+                                                                        initial_collision, initial_traj,
+                                                                        'time'))
+    container.register_quality_table(e_table, e_table.lookup_value('speed-factor', initial_speed_factor, 'time'))
+elif args.type == "valve_walk":
+    initial_dx = 10
+    initial_collision = 10
+    initial_traj = 20
+    initial_speed_factor = 5
+    steps = args.distance / 0.2
+    k = 10
+    distance_factor = 1.0 / (1.0 + k * steps * steps)
+    print "distance_factor:", distance_factor
+    p0_table = QualityTable("q_{p0}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", args.p0 * distance_factor)
+    p1_table = QualityTable("q_{p1}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", 1.0)
+    m0_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q_mono.csv", args.m0 * distance_factor)
+    m1_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q_mono.csv", args.m0)
+    m_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q.csv", args.m0)
+    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/valve/jaxon_red_valve_zmp_ee_normalized.csv", 1)
+    container.register_quality_table(p0_table, p0_table.lookup_value('dx', initial_dx, 'time'))
+    container.register_quality_table(p1_table, p1_table.lookup_value('dx', initial_dx, 'time'))
+    container.register_quality_table(m0_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
+                                                                         initial_collision, initial_traj,
+                                                                         'time'))
+    container.register_quality_table(m1_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
+                                                                         initial_collision, initial_traj,
+                                                                         'time'))
+    container.register_quality_table(e_table, e_table.lookup_value('speed-factor', initial_speed_factor, 'time'))
+elif args.type == "door_walk":
+    initial_dx = 10
+    initial_collision = 10
+    initial_traj = 20
+    initial_speed_factor = 5
+    steps = args.distance / 0.2
+    k = 10
+    distance_factor = 1.0 / (1.0 + k * steps * steps)
+    print "distance_factor:", distance_factor
+    p0_table = QualityTable("q_{p0}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", args.p0 * distance_factor)
+    p1_table = QualityTable("q_{p1}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", 1.0)
+    m0_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q_mono.csv", args.m0 * distance_factor)
+    m1_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q_mono.csv", args.m0)
+    m_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q.csv", args.m0)
+    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/door/jaxon_red_door_zmp_ee_normalized.csv", 1)
+    container.register_quality_table(p0_table, p0_table.lookup_value('dx', initial_dx, 'time'))
+    container.register_quality_table(p1_table, p1_table.lookup_value('dx', initial_dx, 'time'))
+    container.register_quality_table(m0_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
+                                                                         initial_collision, initial_traj,
+                                                                         'time'))
+    container.register_quality_table(m1_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
+                                                                         initial_collision, initial_traj,
+                                                                         'time'))
+    container.register_quality_table(e_table, e_table.lookup_value('speed-factor', initial_speed_factor, 'time'))
+
+    
 # global variables
 # door
 # perception_csv = "epsilon_plane_sigma.csv"
@@ -95,27 +160,27 @@ ax.set_ylabel('$q$')
 # if args.p_tracking != 0.0:
 #     container.register_quality_table(p_tracking_table, p_tracking_table.lookup_value('d', initial_track_d, 'sigma'))
 # print "Initial sigma:", container.current_sigma()
-if container.current_sigma() < deadline_sigma:
-    # print "Increase sigma"
+if container.current_time() < deadline_time:
+    # print "Increase time"
     container.setDirectionPositve()
 else:
-    # print "Decrease sigma"
+    # print "Decrease time"
     container.setDirectionNegative()
 if args.wait:
     raw_input()
 
-while not container.is_converged(deadline_sigma):
+while not container.is_converged(deadline_time):
     if not container.proc():
         container.draw(ax)
         break
     container.draw(ax)
     if args.incremental:
-        container.printOverview2(deadline_sigma)
+        container.printOverview2(deadline_time)
     if args.wait:
         raw_input()
 if not args.incremental:
     if args.quiet:
-        container.printOverview2(deadline_sigma)
+        container.printOverview2(deadline_time)
     else:
-        container.printOverview(deadline_sigma)
+        container.printOverview(deadline_time)
         raw_input()
--- jsk_2015_06_hrp_drc/drc_task_common/src/drc_task_common/optimization_container.py
+++ jsk_2015_06_hrp_drc/drc_task_common/src/drc_task_common/optimization_container.py
@@ -18,16 +18,16 @@ def resolve_ros_path(path):
         return path
 
 class QualityTable():
-    def __init__(self, name, file_name, epsilon_scale):
+    def __init__(self, name, file_name, q_scale):
         self.name = name
         self.file_name = resolve_ros_path(file_name)
-        self.epsilon_scale = epsilon_scale
+        self.q_scale = q_scale
         # read value
         # print "Reading", file_name
         with open(self.file_name, "r") as f:
             reader = csv.reader(f)
             self.labels = reader.next()
-            # self.data["epsilon"] => [0, 0.1, ..., 1.0]
+            # self.data["q"] => [0, 0.1, ..., 1.0]
             self.data = dict()
             for label in self.labels:
                 self.data[label] = []
@@ -36,36 +36,36 @@ class QualityTable():
                     label = self.labels[i]
                     self.data[label].append(float(row[i]))
         # initialize linear interpolator
-        sigmas = np.array(self.data['sigma'])
-        epsilons = np.array(self.data['epsilon'])
-        keys = sorted(range(0, len(sigmas)), key=lambda x: sigmas[x])
-        self.sigmas_sorted = np.array([sigmas[x] for x in keys])
-        self.epsilons_sorted = np.array([epsilons[x] for x in keys])
-        self.sigma2epsilon_interpolation = interpolate.interp1d(
-            self.sigmas_sorted, self.epsilons_sorted, kind="linear")
-        self.epsilon2sigma_interpolation = interpolate.interp1d(
-            self.epsilons_sorted, self.sigmas_sorted, kind="linear")
+        times = np.array(self.data['time'])
+        qs = np.array(self.data['q'])
+        keys = sorted(range(0, len(times)), key=lambda x: times[x])
+        self.times_sorted = np.array([times[x] for x in keys])
+        self.qs_sorted = np.array([qs[x] for x in keys])
+        self.time2q_interpolation = interpolate.interp1d(
+            self.times_sorted, self.qs_sorted, kind="linear")
+        self.q2time_interpolation = interpolate.interp1d(
+            self.qs_sorted, self.times_sorted, kind="linear")
     def plot(self, c):
-        range = np.linspace(self.min_sigma(), self.max_sigma(), 100)
-        plt.plot(range, self.epsilon_scale * self.sigma2epsilon_interpolation(range),
-                 label='${1:.2f}{0}$'.format(self.name, self.epsilon_scale),
+        range = np.linspace(self.min_time(), self.max_time(), 100)
+        plt.plot(range, self.q_scale * self.time2q_interpolation(range),
+                 label='${1:.2f}{0}$'.format(self.name, self.q_scale),
                  c=c)
-    def min_sigma(self):
-        return self.sigmas_sorted[0]
-    def max_sigma(self):
-        return self.sigmas_sorted[-1]
-    def min_epsilon(self):
-        return self.epsilon_scale * self.epsilons_sorted[0]
-    def max_epsilon(self):
-        return self.epsilon_scale * self.epsilons_sorted[-1]
-    def sigma2epsilon(self, sigma):
-        return self.epsilon_scale * self.sigma2epsilon_interpolation(sigma)
-    def epsilon2sigma(self, epsilon):
-        return 1.0 / self.epsilon_scale * self.epsilon2sigma_interpolation(epsilon)
-    def delta(self, sigma, delta_sigma):
-        return self.sigma2epsilon(sigma + delta_sigma) - self.sigma2epsilon(sigma)
+    def min_time(self):
+        return self.times_sorted[0]
+    def max_time(self):
+        return self.times_sorted[-1]
+    def min_q(self):
+        return self.q_scale * self.qs_sorted[0]
+    def max_q(self):
+        return self.q_scale * self.qs_sorted[-1]
+    def time2q(self, time):
+        return self.q_scale * self.time2q_interpolation(time)
+    def q2time(self, q):
+        return 1.0 / self.q_scale * self.q2time_interpolation(q)
+    def delta(self, time, delta_time):
+        return self.time2q(time + delta_time) - self.time2q(time)
     def other_params(self):
-        return [label for label in self.labels if label != "sigma" and label != "epsilon"]
+        return [label for label in self.labels if label != "time" and label != "q"]
     def lookup_value(self, from_label, from_value, target_label):
         # run linear interpolation
         from_data = np.array(self.data[from_label])
@@ -86,54 +86,54 @@ class OptimizationContainer():
     def __init__(self):
         self.iteration_times = 0
         self.tables = []
-        self.initial_sigmas = []
-        self.current_sigmas = []
-        self.tracking_sigmas = []
-    def register_quality_table(self, table, initial_sigma):
+        self.initial_times = []
+        self.current_times = []
+        self.tracking_times = []
+    def register_quality_table(self, table, initial_time):
         self.tables.append(table)
-        self.initial_sigmas.append(initial_sigma)
-        self.current_sigmas.append(initial_sigma)
-        self.tracking_sigmas.append([initial_sigma])
-    def possible_min_sigma(self):
-        return sum([t.min_sigma() for t in self.tables])
-    def current_sigma(self):
-        return sum(self.current_sigmas)
-    def current_epsilon(self):
-        return sum([t.sigma2epsilon(s) for t, s in zip(self.tables, self.current_sigmas)])
-    def initial_sigma(self):
-        return sum(self.initial_sigmas)
-    def initial_epsilon(self):
-        return sum([t.sigma2epsilon(s) for t, s in zip(self.tables, self.initial_sigmas)])
-    def max_epsilon(self):
-        return sum([t.max_epsilon() for t in self.tables])
-    def is_sigma_bigger(self, sigma):
-        return self.current_sigma() < sigma
+        self.initial_times.append(initial_time)
+        self.current_times.append(initial_time)
+        self.tracking_times.append([initial_time])
+    def possible_min_time(self):
+        return sum([t.min_time() for t in self.tables])
+    def current_time(self):
+        return sum(self.current_times)
+    def current_q(self):
+        return sum([t.time2q(s) for t, s in zip(self.tables, self.current_times)])
+    def initial_time(self):
+        return sum(self.initial_times)
+    def initial_q(self):
+        return sum([t.time2q(s) for t, s in zip(self.tables, self.initial_times)])
+    def max_q(self):
+        return sum([t.max_q() for t in self.tables])
+    def is_time_bigger(self, time):
+        return self.current_time() < time
     def setDirectionPositve(self):
         self.direction = 1
     def setDirectionNegative(self):
         self.direction = -1
-    def delta_sigma(self):
+    def delta_time(self):
         # return 0.1 * self.direction
         return 0.3 * self.direction
-    def is_converged(self, sigma_d):
+    def is_converged(self, time_d):
         if self.direction == 1:
-            return sigma_d < self.current_sigma()
+            return time_d < self.current_time()
         else:
-            return sigma_d > self.current_sigma()
-    def is_sigma_valid(self, tp, sigma):
+            return time_d > self.current_time()
+    def is_time_valid(self, tp, time):
         for t in self.tables:
             if t.name == tp:
-                return sigma >= t.min_sigma() and sigma < t.max_sigma()
+                return time >= t.min_time() and time < t.max_time()
     def proc(self):
         self.iteration_times = self.iteration_times + 1
         # print "k=", self.iteration_times
-        delta_sigma = self.delta_sigma()
-        # check is valid sigma
+        delta_time = self.delta_time()
+        # check is valid time
         ds = []
         is_valid = False
-        for t, current_sigma in zip(self.tables, self.current_sigmas):
-            if self.is_sigma_valid(t.name, current_sigma + delta_sigma):
-                d = t.delta(current_sigma, delta_sigma)
+        for t, current_time in zip(self.tables, self.current_times):
+            if self.is_time_valid(t.name, current_time + delta_time):
+                d = t.delta(current_time, delta_time)
                 ds.append(abs(d))
                 is_valid = True
             else:
@@ -148,47 +148,47 @@ class OptimizationContainer():
         if self.direction < 0:
             min_d = ds[keys[0]]
             min_table = self.tables[keys[0]]
-            self.current_sigmas[keys[0]] = self.current_sigmas[keys[0]] + delta_sigma
-            self.tracking_sigmas[keys[0]].append(self.current_sigmas[keys[0]])
+            self.current_times[keys[0]] = self.current_times[keys[0]] + delta_time
+            self.tracking_times[keys[0]].append(self.current_times[keys[0]])
         else:
             min_d = ds[keys[-1]]
             min_table = self.tables[keys[-1]]
-            self.current_sigmas[keys[-1]] = self.current_sigmas[keys[-1]] + delta_sigma
-            self.tracking_sigmas[keys[-1]].append(self.current_sigmas[keys[-1]])
+            self.current_times[keys[-1]] = self.current_times[keys[-1]] + delta_time
+            self.tracking_times[keys[-1]].append(self.current_times[keys[-1]])
         # print "choose", min_table.name
         return True
     def printOverview2(self, deadline):
-        print "{0},{1}".format(self.current_sigma(), self.current_epsilon())
+        print "{0},{1}".format(self.current_time(), self.current_q())
     def printOverview(self, deadline):
         print "initial condition:"
-        print "  all sigma:", self.initial_sigma()
-        print "  all epsilon:", self.initial_epsilon()
-        for t, s in zip(self.tables, self.initial_sigmas):
+        print "  all time:", self.initial_time()
+        print "  all q:", self.initial_q()
+        for t, s in zip(self.tables, self.initial_times):
             print "  " + t.name
-            print "    sigma:", s
-            print "    epsilon:", t.sigma2epsilon(s)
+            print "    time:", s
+            print "    q:", t.time2q(s)
             for p in t.other_params():
-                print "    {0}: {1}".format(p, t.lookup_value("sigma",s, p))
-        print "deadline sigma:", deadline
-        print "minimum sigma:", sum([t.min_sigma() for t in self.tables])
+                print "    {0}: {1}".format(p, t.lookup_value("time",s, p))
+        print "deadline time:", deadline
+        print "minimum time:", sum([t.min_time() for t in self.tables])
         print "final result:"
-        print "  all sigma:", self.current_sigma()
-        print "  all epsilon:", self.current_epsilon()
-        print "  rel epsilon", self.current_epsilon() / self.max_epsilon()
-        for t, s in zip(self.tables, self.current_sigmas):
+        print "  all time:", self.current_time()
+        print "  all q:", self.current_q()
+        print "  rel q", self.current_q() / self.max_q()
+        for t, s in zip(self.tables, self.current_times):
             print "  " + t.name
-            print "    sigma:", s
-            print "    epsilon:", t.sigma2epsilon(s)
+            print "    time:", s
+            print "    q:", t.time2q(s)
             for p in t.other_params():
-                print "    {0}: {1}".format(p, t.lookup_value("sigma",s, p))
+                print "    {0}: {1}".format(p, t.lookup_value("time",s, p))
     def draw(self, ax):
         ax.cla()
         ax.set_xlabel('$t$', fontsize=22)
         ax.set_ylabel('$q$', fontsize=22)
-        max_sigma = max([t.max_sigma() for t in self.tables])
-        max_epsilon = max([t.max_epsilon() for t in self.tables])
-        for t, current_sigma, trackings, c in zip(self.tables, self.current_sigmas,
-                                                  self.tracking_sigmas,
+        max_time = max([t.max_time() for t in self.tables])
+        max_q = max([t.max_q() for t in self.tables])
+        for t, current_time, trackings, c in zip(self.tables, self.current_times,
+                                                  self.tracking_times,
                                                   ["#1f77b4",
                                                    "#ff7f0e",
                                                    "#2ca02c",
@@ -199,17 +199,17 @@ class OptimizationContainer():
                                                    "#7f7f7f",
                                                    "#bcbd22",
                                                    "#17becf"]):
-            current_epsilon = t.sigma2epsilon(current_sigma)
+            current_q = t.time2q(current_time)
             t.plot(c)
             plt.plot(trackings,
-                     t.sigma2epsilon(trackings),
+                     t.time2q(trackings),
                      'o', c=c)
-        ax.set_xlim(0, max_sigma)
-        ax.set_ylim(0, max_epsilon)
+        ax.set_xlim(0, max_time)
+        ax.set_ylim(0, max_q)
         ax.grid()
         plt.legend(loc=4)
-        # plt.text(max_sigma * 5.0 / 8, max_epsilon * 0.1,
-        #          "$k={2}$\n$\epsilon = {0:.1f}$\n$\sigma = {1:.1f}$".format(self.current_epsilon(), self.current_sigma(),
+        # plt.text(max_time * 5.0 / 8, max_q * 0.1,
+        #          "$k={2}$\n$\q = {0:.1f}$\n$\time = {1:.1f}$".format(self.current_q(), self.current_time(),
         #                                                                     self.iteration_times),
         #          ha='left', va='center')
         plt.draw()
