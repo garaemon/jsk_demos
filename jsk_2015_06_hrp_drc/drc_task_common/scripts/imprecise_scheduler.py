54aa4b681ca0b07ebd25e1b806f028ab702d6b13
Ryohei Ueda <garaemon@gmail.com>
2016-01-07 22:27:36 +0900

    [drc_task_common] Add valve_walk2 and door_walk2 models

--- jsk_2015_06_hrp_drc/drc_task_common/scripts/imprecise_scheduler.py
+++ jsk_2015_06_hrp_drc/drc_task_common/scripts/imprecise_scheduler.py
@@ -17,9 +17,9 @@ parser.add_argument('--type', default="valve_inplace", type=str)
 parser.add_argument('--p0', default=1.0, type=float)
 parser.add_argument('--m0', default=1.0, type=float)
 parser.add_argument('--e0', default=4.0, type=float)
-parser.add_argument('--p1', default=1.0, type=float)
+parser.add_argument('--p1', default=0.5, type=float)
 parser.add_argument('--p-tracking', default=0.0, type=float)
-parser.add_argument('--m1', default=1.0, type=float)
+parser.add_argument('--m1', default=0.5, type=float)
 parser.add_argument('--e1', default=1.0, type=float)
 parser.add_argument('time', type=float)
 parser.add_argument('--wait', action="store_true")
@@ -29,10 +29,11 @@ parser.add_argument('--distance', type=float)
 args = parser.parse_args()
 deadline_time = args.time
 
-if args.type not in ["valve_inplace", "door_inplace", "valve_walk", "door_walk"]:
+if args.type not in ["valve_inplace", "door_inplace", "valve_walk", "door_walk", "valve_walk2", "door_walk2"]:
     raise Exception("Unknown type: %s" % (args.type))
 
 container = OptimizationContainer()
+k = 1.0                         # for step cost
 if args.type == "valve_inplace":
     initial_dx = 10
     initial_collision = 10
@@ -67,21 +68,62 @@ elif args.type == "valve_walk":
     initial_traj = 20
     initial_speed_factor = 5
     steps = args.distance / 0.2
-    k = 10
+    distance_factor = 1.0 / (1.0 + k * steps * steps)
+    print "distance_factor:", distance_factor
+    p0_table = QualityTable("q_{p0}", "package://drc_task_common/profile_data/recognition/valve_detection.csv", args.p0 * distance_factor)
+    m0_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_stand2_average_mono.csv", args.m0 * distance_factor)
+    m0_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_stand2_average.csv", args.m0  * distance_factor)
+    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/valve/jaxon_red_valve_zmp_ee_normalized.csv", 1)
+    container.register_quality_table(p0_table, p0_table.lookup_value('dx', initial_dx, 'time'))
+    container.register_quality_table(m0_table, m0_all_table.lookup_value2('collision-num', 'trajectory-num',
+                                                                         initial_collision, initial_traj,
+                                                                         'time'))
+    container.register_quality_table(e_table, e_table.lookup_value('speed-factor', initial_speed_factor, 'time'))
+elif args.type == "valve_walk2":
+    initial_dx = 10
+    initial_collision = 10
+    initial_traj = 20
+    initial_speed_factor = 5
+    steps = args.distance / 0.2
+    distance_factor = 1.0 / (1.0 + k * steps * steps)
+    print "distance_factor:", distance_factor
+    p0_table = QualityTable("q_{p0}", "package://drc_task_common/profile_data/recognition/valve_detection.csv", args.p0 * distance_factor)
+    p1_table = QualityTable("q_{p1}", "package://drc_task_common/profile_data/recognition/valve_detection.csv", 1.0)
+    m0_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_stand2_average_mono.csv", args.m0 * distance_factor)
+    m1_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q_mono.csv", args.m1)
+    m1_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q.csv", args.m1)
+    m0_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_stand2_average.csv", args.m0  * distance_factor)
+    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/valve/jaxon_red_valve_zmp_ee_normalized.csv", 1)
+    container.register_quality_table(p0_table, p0_table.lookup_value('dx', initial_dx, 'time'))
+    container.register_quality_table(p1_table, p1_table.lookup_value('dx', initial_dx, 'time'))
+    container.register_quality_table(m0_table, m0_all_table.lookup_value2('collision-num', 'trajectory-num',
+                                                                         initial_collision, initial_traj,
+                                                                         'time'))
+    container.register_quality_table(m1_table, m1_all_table.lookup_value2('collision-num', 'trajectory-num',
+                                                                         initial_collision, initial_traj,
+                                                                         'time'))
+    container.register_quality_table(e_table, e_table.lookup_value('speed-factor', initial_speed_factor, 'time'))
+elif args.type == "door_walk2":
+    initial_dx = 10
+    initial_collision = 10
+    initial_traj = 20
+    initial_speed_factor = 5
+    steps = args.distance / 0.2
     distance_factor = 1.0 / (1.0 + k * steps * steps)
     print "distance_factor:", distance_factor
     p0_table = QualityTable("q_{p0}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", args.p0 * distance_factor)
     p1_table = QualityTable("q_{p1}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", 1.0)
-    m0_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q_mono.csv", args.m0 * distance_factor)
-    m1_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q_mono.csv", args.m0)
-    m_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_valve_ik_average_q.csv", args.m0)
-    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/valve/jaxon_red_valve_zmp_ee_normalized.csv", 1)
+    m0_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q_mono.csv", args.m0 * distance_factor)
+    m1_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q_mono.csv", args.m1)
+    m0_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_stand2_average.csv", args.m0 * distance_factor)
+    m1_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q.csv", args.m1)
+    e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/door/jaxon_red_door_zmp_ee_normalized.csv", 1)
     container.register_quality_table(p0_table, p0_table.lookup_value('dx', initial_dx, 'time'))
     container.register_quality_table(p1_table, p1_table.lookup_value('dx', initial_dx, 'time'))
-    container.register_quality_table(m0_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
+    container.register_quality_table(m0_table, m0_all_table.lookup_value2('collision-num', 'trajectory-num',
                                                                          initial_collision, initial_traj,
                                                                          'time'))
-    container.register_quality_table(m1_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
+    container.register_quality_table(m1_table, m1_all_table.lookup_value2('collision-num', 'trajectory-num',
                                                                          initial_collision, initial_traj,
                                                                          'time'))
     container.register_quality_table(e_table, e_table.lookup_value('speed-factor', initial_speed_factor, 'time'))
@@ -91,21 +133,14 @@ elif args.type == "door_walk":
     initial_traj = 20
     initial_speed_factor = 5
     steps = args.distance / 0.2
-    k = 10
     distance_factor = 1.0 / (1.0 + k * steps * steps)
     print "distance_factor:", distance_factor
     p0_table = QualityTable("q_{p0}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", args.p0 * distance_factor)
-    p1_table = QualityTable("q_{p1}", "package://drc_task_common/profile_data/recognition/epsilon_plane_sigma.csv", 1.0)
     m0_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q_mono.csv", args.m0 * distance_factor)
-    m1_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q_mono.csv", args.m0)
-    m_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_average_q.csv", args.m0)
+    m0_all_table = QualityTable("q_{m0}", "package://drc_task_common/profile_data/motion/jaxon_door_ik_stand2_average.csv", args.m0 * distance_factor)
     e_table = QualityTable("q_{e0}", "package://drc_task_common/profile_data/execution/door/jaxon_red_door_zmp_ee_normalized.csv", 1)
     container.register_quality_table(p0_table, p0_table.lookup_value('dx', initial_dx, 'time'))
-    container.register_quality_table(p1_table, p1_table.lookup_value('dx', initial_dx, 'time'))
-    container.register_quality_table(m0_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
-                                                                         initial_collision, initial_traj,
-                                                                         'time'))
-    container.register_quality_table(m1_table, m_all_table.lookup_value2('collision-num', 'trajectory-num',
+    container.register_quality_table(m0_table, m0_all_table.lookup_value2('collision-num', 'trajectory-num',
                                                                          initial_collision, initial_traj,
                                                                          'time'))
     container.register_quality_table(e_table, e_table.lookup_value('speed-factor', initial_speed_factor, 'time'))
